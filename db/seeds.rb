e0db08a02221a49ba715d2726a492dbc381d71c7
Francis Thometz <francis.thometz@gmail.com>
2018-11-20 16:49:50 +0900

    Added styling to my Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  table = Article.new(title: Faker::BojackHorseman.character, content: Faker::BojackHorseman.quote)
+  table.save
+end
